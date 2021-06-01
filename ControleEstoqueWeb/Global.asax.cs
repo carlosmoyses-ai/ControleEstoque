828910816f22d352c1861f3e29e2115ba8268999
Carlos Moyses <carlosr.2019@alunos.utfpr.edu.br>
2021-05-31 21:47:37 -0300

    segurancao Parte 3

--- ControleEstoqueWeb/Global.asax.cs
+++ ControleEstoqueWeb/Global.asax.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,6 +14,27 @@ namespace ControleEstoqueWeb
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+
+            if (ex is HttpRequestValidationException)
+            {
+                Response.Clear();
+                Response.StatusCode = 200;
+                Response.ContentType = "application/json";
+                Response.Write("{ \"Resultado\":\"AVISO\",\"Mensagens\":[\"Somente texto sem caracteres especiais pode ser enviado.\"],\"IdSalvo\":\"\"}");
+                Response.End();
+            }
+            else if (ex is HttpAntiForgeryException)
+            {
+                Response.Clear();
+                Response.StatusCode = 200;
+                Response.End();
+                // gravar LOG
+            }
         }
     }
 }
